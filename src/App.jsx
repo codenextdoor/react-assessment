92b620d3515368915810674e7344ae552b3aa925
codenextdoor <xuluv911@gmail.com>
2023-03-24 09:52:18 +0545

    shopping-list commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
 import "./App.css";
 import { ShoppingListItem } from "./components/ShoppingListItem";
 
 function App() {
+  const [list, setList] = useState([]);
+  const [newItem, setNewItem] = useState("");
+
+  const handleAdd = () => {
+    if (!newItem.trim()) return; // Prevent empty string
+    if (list.find((item) => item.name === newItem.trim())) {
+      alert("Item already exists"); // Prevent duplicates
+      return;
+    }
+    setList([
+      ...list,
+      { id: Date.now(), name: newItem.trim(), checked: false },
+    ]);
+    setNewItem("");
+  };
+
+  const handleRemove = (id) => {
+    setList(list.filter((item) => item.id !== id));
+  };
+
+  const handleToggle = (id) => {
+    setList(
+      list.map((item) =>
+        item.id === id ? { ...item, checked: !item.checked } : item
+      )
+    );
+  };
+
+  const handleInputChange = (event) => {
+    setNewItem(event.target.value);
+  };
+
+  const handleInputKeyDown = (event) => {
+    if (event.keyCode === 13) {
+      handleAdd();
+    }
+  };
   return (
     <div className="container">
       <h1 className="mb-4">My Shopping List</h1>
@@ -11,13 +49,28 @@ function App() {
           type="text"
           placeholder="E.g. Carrots"
           className="v__input flex-1"
+          value={newItem}
+          onChange={handleInputChange}
+          onKeyDown={handleInputKeyDown}
         />
-        <button className="v__button">Add</button>
+        <button
+          className="v__button"
+          onClick={handleAdd}
+          disabled={!newItem.trim()}
+        >
+          Add
+        </button>
       </div>
       <div className="v__list-container overflow-y-scroll">
         {/* Map your data here: */}
-        <ShoppingListItem />
-        <ShoppingListItem />
+        {list.map((item) => (
+          <ShoppingListItem
+            key={item.id}
+            item={item}
+            onRemove={handleRemove}
+            onToggle={handleToggle}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/ShoppingListItem.jsx
+++ src/components/ShoppingListItem.jsx
@@ -1,11 +1,32 @@
 import classes from "./ShoppingListItem.module.css";
 
-export const ShoppingListItem = () => {
+export const ShoppingListItem = ({ item, onRemove, onToggle }) => {
+  const handleRemove = () => {
+    onRemove(item.id);
+  };
+
+  const handleToggle = () => {
+    onToggle(item.id);
+  };
+
   return (
     <div className="flex items-center p-2">
-      <input type="checkbox" className="mr-2" />
-      <h3 className="flex-1">Carrots</h3>
-      <button className={classes.removeButton}>x</button>
+      <input
+        type="checkbox"
+        className="mr-2"
+        checked={item && item.checked}
+        onChange={handleToggle}
+      />
+
+      <h3
+        className={`flex-1 ${item.checked ? classes.checked : ""}`}
+        style={{ textDecoration: item.checked ? "line-through" : "none" }}
+      >
+        {item && item.name}
+      </h3>
+      <button className={classes.removeButton} onClick={handleRemove}>
+        x
+      </button>
     </div>
   );
 };
